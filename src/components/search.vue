0455f156477bd350d461f7c123cfd319d71e3048
Nestor hdez <nestorhdez.26@gmail.com>
2019-08-24 18:05:52 +0100

    Make search component resposive.

--- src/components/search.vue
+++ src/components/search.vue
@@ -24,14 +24,30 @@ export default {
   height: 30px;
   margin-right: 15px;
   padding: 0 15px;
-  border-radius: 10px;
+  border-radius: 3px;
   border: 1px solid #d7d5d5;
 }
 #search-btn {
   width: 90px;
   height: 30px;
-  border-radius: 10px;
+  border-radius: 3px;
   border-color: #d7d5d5;
   background-color: white;
+}
+
+@media (max-width: 500px) {
+  #search-container {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+  #search-input {
+    width: 250px;
+    margin-right: 0;
+    margin-bottom: 15px;
+  }
+  #search-btn {
+    width: 250px;
+  }
 }
 </style>
